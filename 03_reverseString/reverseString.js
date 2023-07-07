9472a1449281185bd4ae6edecacba3d0caae39a0
Jacob Rees <reescodes@gmail.com>
2023-07-06 19:22:07 -0600

    Finish exercise 03, reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let new_string = '';
+    for (let i = -1; i < string.length + 1; i++) {
+        new_string += string.charAt(string.length-i)
+    }
+    return new_string;
 };
 
 // Do not edit below this line
